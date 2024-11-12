c1a620715c49ddd7747625931bd2da34edb7e733
SebastianAval0s <lashadassd60@gmail.com>
2024-11-11 22:07:50 -0500

    Add files via upload

---
+++ ParaZarahi/script .js
@@ -0,0 +1,7 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+alert  ("Sebastian Avalos dice:\n\n ¿Aceptas estas lindas flores 😊?")
+
+alert ("Para ti las hermosas flores 🤗")
